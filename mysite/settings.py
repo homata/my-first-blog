98b0b077d7678c84b2a18e44188aee282078c8a4
Hiroshi Omata <op.homata@gmail.com>
2018-08-26 15:21:59 +0900

    Update settings parameter is tokyo

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = '$#*_)av@b)g6i6ub0h0ub$flxv_$(5&6=ph*tn$^afpz2vxw$h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 # Application definition
 
@@ -103,9 +102,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
@@ -118,3 +117,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
